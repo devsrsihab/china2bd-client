305d62097f9b33e76b9e906c5ca195ea557d5ac8
Md Sohanur Rohman Sihab <srsohankhan0@gmail.com>
2025-09-15 16:53:41 +0600

    image path testing

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -36,13 +36,14 @@ const ProductCard: React.FC<ProductCardProps> = ({
         <CardContent className="p-0 flex flex-col items-center">
           {/* Image Container */}
           <div className="prod-image-container overflow-hidden rounded-2xl w-full">
-            <span>testing update 2</span>
-            <img
+            <span>testing update 1</span>
+            <Image
               width={223}
               height={230}
               className="product-image w-full  h-[230px] object-cover object-center bg-white rounded-xl border border-gray-100 mb-1.5 transition-all duration-200 ease-in-out"
               src={imageSrc}
               alt={imageAlt}
+              loading="lazy"
             />
           </div>
           {/* Product Details (Name, Price, Sold) */}
